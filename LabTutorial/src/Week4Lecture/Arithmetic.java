75b34608591eb597009e74471c9050bd09cd7785
caizhen <zhenchai0000@gmail.com>
2023-10-11 21:02:25 +0800

    add: tutorial sample code and exercise

---
+++ LabTutorial/src/Week4Lecture/Arithmetic.java
@@ -0,0 +1,19 @@
+package Week4Lecture;
+
+public class Arithmetic {
+	public static void main (String[] args) {
+		// arthimatic
+		System.out.println(1 + 1); // overloaded
+		System.out.println("hello" + 1 + 1);
+		System.out.println("hello" + (1 + 1));
+		
+		int a = 99 / 10;
+		System.out.println(a);
+		
+		double b = 99 / 10;
+		System.out.println(b);
+		
+		double c = 99.0 / 10.0;
+		System.out.println(c);
+	}
+}
---
+++ LabTutorial/src/Week4Lecture/Main.java
@@ -0,0 +1,7 @@
+package Week4Lecture;
+
+public class Main {
+	public static void main (String[] args) {
+		Arithmetic a = new Arithmetic();
+	}
+}
